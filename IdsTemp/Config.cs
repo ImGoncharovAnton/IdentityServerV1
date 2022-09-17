aaa54f786137defa943b323d29e7437c9c6d71b2
Anton Goncharov <gold-library@yandex.ru>
2022-09-17 10:06:07 +0300

    Initial

---
+++ IdsTemp/Config.cs
@@ -0,0 +1,62 @@
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+using IdentityModel;
+
+namespace IdsTemp;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+        {
+            new ApiScope("inventory", "Inventory")
+        };
+
+    public static IEnumerable<Client> Clients =>
+        new Client[]
+        {
+            // m2m client credentials flow client
+            new Client
+            {
+                // machine to machine client
+                ClientId = "inventory-api",
+                ClientSecrets = { new Secret("secret".Sha256()) },
+            
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                // scopes that client has access to
+                AllowedScopes = { "inventory" }
+            },
+
+            // interactive SPA angular
+            new Client
+            {
+                ClientName = "Angular-client",
+                ClientId = "angular-client",
+                ClientSecrets = { new Secret("7A622350-FB10-427C-BCC1-723253A4B3FD".Sha256())},
+                AllowedGrantTypes = GrantTypes.Code,
+                RedirectUris = new List<string> { "http://localhost:4200/signin-callback", "http://localhost:4200/assets/silent-callback.html" },
+                RequirePkce = true,
+                AllowAccessTokensViaBrowser = true,
+                AllowedScopes =
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    "inventory",
+                    JwtClaimTypes.Role
+                },
+                AllowedCorsOrigins = { "http://localhost:4200" },
+                RequireClientSecret = false,
+                AllowOfflineAccess = true,
+                PostLogoutRedirectUris = new List<string> { "http://localhost:4200/signout-callback" },
+                RequireConsent = false,
+                AccessTokenLifetime = 1800
+            }
+        };
+}
---
+++ IdsTemp/HostingExtensions.cs
@@ -0,0 +1,117 @@
+using Duende.IdentityServer;
+using IdsTemp.Data;
+using IdsTemp.Extensions;
+using IdsTemp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
+using Serilog;
+
+namespace IdsTemp;
+
+internal static class HostingExtensions
+{
+    public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddRazorPages();
+
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+        
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(connectionString));
+
+        builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
+        builder.Services
+            .AddIdentityServer(options =>
+            {
+                options.Events.RaiseErrorEvents = true;
+                options.Events.RaiseInformationEvents = true;
+                options.Events.RaiseFailureEvents = true;
+                options.Events.RaiseSuccessEvents = true;
+
+                // see https://docs.duendesoftware.com/identityserver/v6/fundamentals/resources/
+                options.EmitStaticAudienceClaim = true;
+            })
+            .AddConfigurationStore(options =>
+            {
+                options.ConfigureDbContext = b =>
+                    b.UseSqlServer(connectionString,
+                        dbOpts => dbOpts.MigrationsAssembly(typeof(Program).Assembly.FullName));
+            })
+            // this is something you will want in production to reduce load on and requests to the DB
+            //.AddConfigurationStoreCache()
+            //
+            // this adds the operational data from DB (codes, tokens, consents)
+            .AddOperationalStore(options =>
+            {
+                options.ConfigureDbContext = b =>
+                    b.UseSqlServer(connectionString,
+                        dbOpts => dbOpts.MigrationsAssembly(typeof(Program).Assembly.FullName));
+
+                // this enables automatic token cleanup. this is optional.
+                options.EnableTokenCleanup = true;
+                options.RemoveConsumedTokens = true;
+            })
+            .AddAspNetIdentity<ApplicationUser>()
+            .AddProfileService<CustomProfileService>();;
+
+        // builder.Services.AddAuthentication()
+        //     .AddGoogle(options =>
+        //     {
+        //         options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+        //
+        //         // register your IdentityServer with Google at https://console.developers.google.com
+        //         // enable the Google+ API
+        //         // set the redirect URI to https://localhost:5001/signin-google
+        //         options.ClientId = "copy client ID from Google here";
+        //         options.ClientSecret = "copy client secret from Google here";
+        //     });
+
+        builder.Services.ConfigureApplicationCookie(config =>
+        {
+            config.Cookie.Name = "IdentityServer.Cookie";
+            config.LoginPath = "/Auth/Login";
+            config.LogoutPath = "/Auth/Logout";
+        });
+
+        builder.Services.AddControllersWithViews();
+        
+        return builder.Build();
+    }
+
+    public static WebApplication ConfigurePipeline(this WebApplication app)
+    {
+        app.UseSerilogRequestLogging();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseIdentityServer();
+        app.UseAuthorization();
+
+        app.UseStaticFiles(new StaticFileOptions
+        {
+            FileProvider = new PhysicalFileProvider(
+                Path.Combine(app.Environment.ContentRootPath, "Styles")),
+            RequestPath = "/styles"
+        });
+        
+        //  Не уверен, надо ли
+        app.MapRazorPages()
+            .RequireAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapDefaultControllerRoute();
+        });
+        
+        return app;
+    }
+}
