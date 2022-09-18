e7407d557fc479ee0ca71833110fb3d07aa1dc3f
Anton Goncharov <gold-library@yandex.ru>
2022-09-18 22:08:30 +0300

    complite register and login

--- IdsTemp/Data/Migrations/20220917094205_Change_Application_user.Designer.cs
+++ IdsTemp/Data/Migrations/20220917094205_Change_Application_user.Designer.cs
@@ -4,6 +4,7 @@ using IdsTemp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace IdsTemp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220917094205_Change_Application_user")]
+    partial class Change_Application_user
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
